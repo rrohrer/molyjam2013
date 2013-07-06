e9d2de2cffcbaffbd7986eb61165d619ad2c90ca
Ryan Rohrer <ryan.rohrer@gmail.com>
2013-07-06 01:45:53 -0700

    added framework for renderer, prepped index for three
    removed canvas element from index so that three.js can render it.

---
+++ js/components/RenderManager.js
@@ -0,0 +1,27 @@
+TANK.registerComponent("RenderManager")
+
+.construct(function ()
+{
+
+})
+
+.initialize(function ()
+{
+  //update function for the renderer
+  this.addEventListener("OnEnterFrame", function (dt)
+  {
+
+  });
+
+  //called when a component is added to the engine
+  this.onAddEventListener("OnComponentInitialized", function (component)
+  {
+
+  });
+
+  this.onAddEventListener("OnComponentUninitialized", function (component)
+  {
+
+  });
+})
+
