482f9207c5b713407e05ddd3c71155ecc97d3678
Ryan Rohrer <ryan.rohrer@gmail.com>
2013-07-06 18:17:10 -0700

    player movement
    simple positional movement

--- js/components/Player.js
+++ js/components/Player.js
@@ -11,11 +11,39 @@ TANK.registerComponent("Player")
   {
     if (key == TANK.UP_ARROW)
     {
-      this.parent.Pos3D.addPosition(1,0,0);
+      this.parent.Pos3D.addPosition(0.1,0,0);
     }
     if (key == TANK.DOWN_ARROW)
     {
-      this.parent.Pos3D.addPosition(-1,0,0);
+      this.parent.Pos3D.addPosition(-0.1,0,0);
+    }
+    if (key == TANK.LEFT_ARROW)
+    {
+      this.parent.Pos3D.addPosition(0,0,-0.1);
+    }
+    if (key == TANK.RIGHT_ARROW)
+    {
+      this.parent.Pos3D.addPosition(0,0,0.1);
+    }
+  });
+
+  this.addEventListener("OnKeyHeld", function (key)
+  {
+    if (key == TANK.UP_ARROW)
+    {
+      this.parent.Pos3D.addPosition(0.1,0,0);
+    }
+    if (key == TANK.DOWN_ARROW)
+    {
+      this.parent.Pos3D.addPosition(-0.1,0,0);
+    }
+    if (key == TANK.LEFT_ARROW)
+    {
+      this.parent.Pos3D.addPosition(0,0,-0.1);
+    }
+    if (key == TANK.RIGHT_ARROW)
+    {
+      this.parent.Pos3D.addPosition(0,0,0.1);
     }
   })
 })
